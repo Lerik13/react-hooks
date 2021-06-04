fef1d53c02687036c1cb5e38fea065821e65f590
Valeria Kan <kan.valeria1309@gmail.com>
2021-06-04 11:33:38 -0600

    hook useState in App, added function 'addTodo'

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+import TodoList from './TodoList';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+
+	const [todos, setTodos]  = useState([
+		{id: 1, completed: false, title: 'buy bread'},
+		{id: 2, completed: true, title: 'buy butter'},
+		{id: 3, completed: false, title: 'buy milk'}	
+	]);
+	const [todoTitle, setTodoTitle] = useState('');
+
+	const addTodo = event => {
+		if(event.key === 'Enter') {
+			setTodos([
+				...todos,
+				{
+					id: Date.now(),
+					title: todoTitle,
+					completed: false
+				}
+			]);
+			setTodoTitle('');
+		}
+	};
+
+	return (
+		<div className='container'>
+			<h1>Todo app</h1>
+
+			<div className='input-field'>
+				<input 
+					type='text'
+					value={todoTitle}
+					onChange={event => setTodoTitle(event.target.value)}
+					onKeyPress={addTodo}
+				/>
+				<label>Todo name</label>
+			</div>
+
+			<TodoList todos={todos}/>
+		</div>
+	);
 }
 
-export default App;
